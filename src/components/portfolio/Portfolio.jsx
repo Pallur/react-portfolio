8768e2b3ef9819fefb5a6a08aa56c818a5040bcc
pallipalli <pallbj20@tskoli.is>
2022-05-07 17:56:54 +0000

    aduco 90%

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,80 +1,67 @@
 import "./portfolio.scss"
-import PortfolioList from "../portfolioList/PortfolioList";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export default function Portfolio() {
 
   const [selected, setSelected] = useState("aduco")
 
-  const list = [
-    {
-      id: "aduco",
-      title: "Aduco",
-    },
-    {
-      id: "archviz",
-      title: "Arch Viz",
-    },
-    {
-      id: "art",
-      title: "Art",
-    },
-    {
-      id: "hans",
-      title: "Hans",
-    },
-  ];
-
   return (
     <div className="portfolio" id="portfolio">
-      <h1>Portfolio</h1>
-      <ul>
-        {list.map(item=>(
-          <PortfolioList 
-            title={item.title} 
-            active={selected === item.id} 
-            setSelected={setSelected} 
-            id={item.id}
-          />
-        ))}
-      </ul>
-      <div className="container">
+      <h1>Aduco</h1>
+      
+      <div className="wholeContainer">
+      <div className="imageContainers">
+        <div className="container">
           <div className="item">
             <img 
-              src="" 
+              src="assets/menace_01.png" 
               alt="" 
             />
-            <h3>Eitthvað</h3>
           </div>
+        </div>
+        <div className="container">
+          <div className="item">
+            <img 
+              src="assets/light_01.png" 
+              alt="" 
+            />
+          </div>
+        </div>
+        <div className="container">
+          <div className="item">
+            <img 
+              src="assets/butterfly_01.png" 
+              alt="" 
+            />
+          </div>
+        </div>
+        <div className="container">
+          <div className="item">
+            <img 
+              src="assets/subway_01.png" 
+              alt="" 
+            />
+          </div>
+        </div>
+
+        
+
       </div>
-      <div className="container">
-          <div className="item">
-            <img 
-              src="" 
-              alt="" 
-            />
-            <h3>Eitthvað</h3>
-          </div>
+      <div className="textRight">
+        <text className="infoText"> 
+        Aduco er tölvuleikur gerður í UE4 sem lokaverkafni í RADE 2022. {"\n"}
+        Leikurinn gerist í illa förnum heimi þar sem vélmenni ráða ríkjum. {"\n"}
+        Spilari þarf að ferðast yfir auðna jörð til að finna bróður sinn. {"\n"}
+
+        </text>
+        <h2>
+          <a href="https://p0llur.itch.io/aduco"> Download now! </a>
+        </h2>
       </div>
-      <div className="container">
-          <div className="item">
-            <img 
-              src="" 
-              alt="" 
-            />
-            <h3>Eitthvað</h3>
-          </div>
-      </div>
-      <div className="container">
-          <div className="item">
-            <img 
-              src="" 
-              alt="" 
-            />
-            <h3>Eitthvað</h3>
-          </div>
       </div>
       
+      
     </div>
+    
   )
 }
